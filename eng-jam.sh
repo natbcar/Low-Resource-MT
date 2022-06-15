e7cfe6b16411b23f095071a18fdf1ef196454e85
Nathaniel Romney Robinson <n8rrobinson@gmail.com>
2022-06-15 12:30:46 -0400

    helpful bash scripts

---
+++ eng-jam.sh
@@ -0,0 +1,10 @@
+ENGJAM=/usr1/data/nrrobins/translation/onmt_corpora/eng_jam
+# files: eng-fra.english  eng-fra.french  jam-fra.french  jam-fra.jamaican
+echo "BASE XPER'T %%%%%%"
+python3 mt.py --out-dir en-jm --phon-type base --phon-pad zero --phon-gram 1 --src1 $ENGJAM/jam-fra.jamaican --tgt1 $ENGJAM/jam-fra.french --src2 $ENGJAM/eng-fra.english --tgt2 $ENGJAM/eng-fra.french --src1_lang jam --src2_lang eng --tgt-lang fra --train1-len 5925 --train2-len 250000 --val-len 1000 --test-len 1000 --config-temp config_template --mod-dim 512 --train-steps 60000 --save-steps 30000 --val-steps 5000 --spm-vocab-size 8000
+echo "PHON RAND NGRAM XPER'T %%%%%%"
+python3 mt.py --out-dir en-jm --phon-type phon --phon-pad rand --phon-gram 3 --src1 $ENGJAM/jam-fra.jamaican --tgt1 $ENGJAM/jam-fra.french --src2 $ENGJAM/eng-fra.english --tgt2 $ENGJAM/eng-fra.french --src1_lang jam --src2_lang eng --tgt-lang fra --train1-len 5925 --train2-len 250000 --val-len 1000 --test-len 1000 --config-temp config_template --mod-dim 512 --train-steps 60000 --save-steps 30000 --val-steps 5000 --spm-vocab-size 8000
+echo "PHON RAND 1GRAM XPER'T %%%%%%"
+python3 mt.py --out-dir en-jm --phon-type phon --phon-pad rand --phon-gram 1 --src1 $ENGJAM/jam-fra.jamaican --tgt1 $ENGJAM/jam-fra.french --src2 $ENGJAM/eng-fra.english --tgt2 $ENGJAM/eng-fra.french --src1_lang jam --src2_lang eng --tgt-lang fra --train1-len 5925 --train2-len 250000 --val-len 1000 --test-len 1000 --config-temp config_template --mod-dim 512 --train-steps 60000 --save-steps 30000 --val-steps 5000 --spm-vocab-size 8000
+echo "PHON ZERO NGRAM XPER'T %%%%%%"
+python3 mt.py --out-dir en-jm --phon-type phon --phon-pad zero --phon-gram 3 --src1 $ENGJAM/jam-fra.jamaican --tgt1 $ENGJAM/jam-fra.french --src2 $ENGJAM/eng-fra.english --tgt2 $ENGJAM/eng-fra.french --src1_lang jam --src2_lang eng --tgt-lang fra --train1-len 5925 --train2-len 250000 --val-len 1000 --test-len 1000 --config-temp config_template --mod-dim 512 --train-steps 60000 --save-steps 30000 --val-steps 5000 --spm-vocab-size 8000
